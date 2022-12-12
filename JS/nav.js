97406388d956dbb08ca6e0c9031ad10e26545f53
Florian Kürsten <f.kuersten@hotmail.com>
2022-12-12 11:52:05 +0100

    navbar

---
+++ JS/nav.js
@@ -0,0 +1,17 @@
+$('.navTrigger').click(function () {
+    $(this).toggleClass('active');
+    console.log("Clicked menu");
+    $("#mainListDiv").toggleClass("show_list");
+    $("#mainListDiv").fadeIn();
+
+});
+/* Function used to shrink nav bar removing paddings and adding black background  */
+
+$(window).scroll(function() {
+    if ($(document).scrollTop() > 50) {
+        $('.nav').addClass('affix');
+        console.log("OK");
+    } else {
+        $('.nav').removeClass('affix');
+    }
+});
